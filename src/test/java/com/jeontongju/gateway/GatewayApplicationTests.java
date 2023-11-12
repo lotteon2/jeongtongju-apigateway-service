706b402885a1d9d76bc1590a21b2839305371fd5
최성훈 <33412452+realsuperman@users.noreply.github.com>
2023-11-12 19:51:51 +0900

    first commit

---
+++ src/test/java/com/jeontongju/gateway/GatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jeontongju.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
